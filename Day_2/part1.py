a234f85e861b9be672c3a067a85674811410a8f8
Dávid Soóky <sooky.david98@gmail.com>
2023-12-02 13:56:06 +0100

    Solved

---
+++ Day_2/part1.py
@@ -0,0 +1,60 @@
+file1 = open('input.txt', 'r')
+lines = file1.readlines()
+
+counter = 0
+draws = []
+cubes = {"red": "12",
+         "green": "13",
+         "blue": "14"}
+game_id = []
+
+
+"""
+Compare the set of cubes to the maximum set of cubes
+Return: True or False based on if the set is possible or not
+"""
+
+
+def compare(hand_dict):
+    for key, value in cubes.items():
+        if key in hand_dict:
+            if int(hand_dict[key]) > int(value):
+                return False
+    return True
+
+
+"""
+Divide the game into sets
+Return: True or False based on the compare() function
+"""
+
+
+def divide_game(full_hand):
+    for items in full_hand:
+        items = items.split(" ")
+        items.pop(0)
+        hand = {}
+        for i in range(0, len(items), 2):
+            items[i+1] = items[i+1].replace(",", "")
+            hand[items[i+1]] = items[i]
+        if not compare(hand):
+            return False
+    return True
+
+
+"""
+Main 
+Return: Sum of IDs
+"""
+
+
+for line in lines:
+    line = line[:-1]
+    line = line.split(":")
+    game_id = line[0].split(" ")[1]
+    draws = line[1]
+    draws = draws.split(";")
+    if not divide_game(draws):
+        continue
+    counter += int(game_id)
+print(counter)
---
+++ Day_2/part2.py
@@ -0,0 +1,70 @@
+# Read lines from file
+file1 = open('input.txt', 'r')
+lines = file1.readlines()
+
+# Initialize required variables
+counter = 0
+cubes = {"red": "0",
+         "green": "0",
+         "blue": "0"}
+
+"""
+Calculate the power of the set of cubes
+Return: None
+"""
+
+
+def compare(hand_dict):
+    for key, value in cubes.items():
+        if key in hand_dict:
+            if int(hand_dict[key]) > int(value):
+                cubes[key] = hand_dict[key]
+
+
+"""
+Divide the game into sets
+Return: None
+"""
+
+
+def divide_game(full_hand):
+    for items in full_hand:
+        items = items.split(" ")
+        items.pop(0)
+        hand = {}
+        for i in range(0, len(items), 2):
+            items[i+1] = items[i+1].replace(",", "")
+            hand[items[i+1]] = items[i]
+        compare(hand)
+
+
+"""
+Calculate the power of the set of cubes
+Return: Power of the set
+"""
+
+
+def calculate(hand):
+    result = 1
+    for key, value in hand.items():
+        result *= int(value)
+    return result
+
+
+"""
+Main 
+Return: Sum of every sets power
+"""
+
+
+for line in lines:
+    for key, value in cubes.items():
+        cubes[key] = "0"
+    line = line[:-1]
+    line = line.split(":")
+    game_id = line[0].split(" ")[1]
+    draws = line[1]
+    draws = draws.split(";")
+    divide_game(draws)
+    counter += calculate(cubes)
+print(counter)
